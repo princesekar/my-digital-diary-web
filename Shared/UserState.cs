3e9c5f14cdd80e7ed6a3690e77f15717b6ea9ab2
Ajith R <ajithr@syncfusion.com>
2022-05-26 16:53:51 +0530

    Modified themes layout from page to dialog

--- Shared/UserState.cs
+++ Shared/UserState.cs
@@ -2,10 +2,13 @@
 {
     public class UserState
     {
+        public event Action OnChange;
         public bool IsUserLoggedIn { get; set; }
         public string? LoggedInUserID { get; set; } = null;
 
         public string Theme { get; set; } = MudBlazor.Colors.Indigo.Darken2;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
 
         public async Task SetLogin(bool isLoggedIn, string? userId)
         {
@@ -17,6 +20,7 @@
         public async Task SetTheme(string themeColor)
         {
             Theme = themeColor;
+            NotifyStateChanged();
             await Task.CompletedTask;
         }
     }
